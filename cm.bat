8179de9a2b3039e3a983638e228815fc0f45462c
Mnyomi2 <bayrout2k1x2@outlook.com>
2025-11-09 21:57:14 +0000

    Update cm.bat

--- cm.bat
+++ cm.bat
@@ -2,5 +2,5 @@
     --code="4/0Ab32j90RXtJhwOuMuKIlH-BsgSFiFvT4i97i2JYZ0GV2aO5JkXUFy2zp6-L6tjo-B2ZBEA" ^
     --redirect-url="https://remotedesktop.google.com/_/oauthredirect" ^
     --name=%COMPUTERNAME% ^
-    --pin=123456
+    --pin=000000
 exit
